affd2ec5ce5a211adbcf5e408bb44e578c0f3363
Krzysztof Zalewski <zalewski2010@gmail.com>
2024-05-04 09:17:49 +0200

    zad4

--- jv/src/Main.java
+++ jv/src/Main.java
@@ -36,5 +36,29 @@ public class Main {
         Messenger messengerSum = new EmailMessenger();
         messengerSum.sendMessage("The sum of " + num1 + " and " + num2 + " is: " + sum);
 
+        Person[] people = new Person[5];
+
+        final int b = 10; // stała
+
+        try {
+            people[0] = new Person("John Doe", 30);
+            people[1] = new Person("Joe Smith", 25);
+            people[2] = new Person("John Johnson", 40);
+            people[3] = new Person("Mike Micheal", 35);
+            people[4] = new Person("Michael Mike", 28);
+        } catch (InvalidAgeException e) {
+            System.err.println("Invalid age provided: " + e.getMessage());
+        }
+
+        Messenger messenger3 = new EmailMessenger();
+
+        for (int i = 0; i < people.length; i++) {
+            if (people[i] != null) {
+                int age = people[i].getAge();
+                int value = MathUtils.add(age, b);
+                messenger3.sendMessage(people[i].getName() + " is " + age + " years old. Calculated value is: " + value);
+            }
+        }
+
     }
 }
