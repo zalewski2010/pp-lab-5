2429caf7b22082992796b8696e80fe41e03f8da5
Krzysztof Zalewski <zalewski2010@gmail.com>
2024-05-04 08:49:25 +0200

    zz

--- jv/src/Main.java
+++ jv/src/Main.java
@@ -6,11 +6,19 @@ import company.exceptions.InvalidAgeException;
 public class Main {
     public static void main(String[] args) {
         try {
-            Person person = new Person("Angelina", 130);
-            System.out.println("Name: " + person.getName());
-            System.out.println("Age: " + person.getAge());
+            Person person1 = new Person("Angelina", 30);
+            System.out.println("Name: " + person1.getName());
+            System.out.println("Age: " + person1.getAge());
         } catch (InvalidAgeException e) {
-            System.err.println("Oh no invalid age provided: " + e.getMessage() + " Please provide age under 125");
+            System.err.println("Oh no invalid age provided for person1: " + e.getMessage() + " Please provide age under 125");
+        }
+
+        try {
+            Person person2 = new Person("Marek", 130);
+            System.out.println("Name: " + person2.getName());
+            System.out.println("Age: " + person2.getAge());
+        } catch (InvalidAgeException e) {
+            System.err.println("Oh no invalid age provided for person2: " + e.getMessage() + " Please provide age under 125");
         }
     }
 }
